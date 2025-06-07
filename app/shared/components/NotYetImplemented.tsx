c8211bc8fe92f997c636820ddf03e4b11642e8fa
Angga-XII <prianka.anggara@gmail.com>
2025-06-07 13:05:15 +0700

    fixed footer coloration on dark and light mode and fixed a bug on NotYetImplemented page padding and mobile mode

--- app/shared/components/NotYetImplemented.tsx
+++ app/shared/components/NotYetImplemented.tsx
@@ -1,14 +1,19 @@
 // import { Heading1 } from "./textWrapper";
 import GlitchText from "../animatedComponent/glitchedText/glitchedText";
+import IsMobileHook from "../hooks/isMobileHook";
 import BodyWrapper from "./bodyWrapper/bodyWrapper";
 
 const NotYetImplemented = (props: any) => {
+  const isMobile = IsMobileHook();
+
   return (
-    <BodyWrapper>
-      <GlitchText speed={3} enableShadows={false} enableOnHover={false} className="bg-black">
-        {props.name} is not yet implemented
-      </GlitchText>
-    </BodyWrapper>
+    <div className="py-100">
+      <BodyWrapper isMobile={isMobile}>
+        <GlitchText speed={3} enableShadows={false} enableOnHover={false} className="bg-black">
+          {props.name} is not yet implemented
+        </GlitchText>
+      </BodyWrapper>
+    </div>
   );
 };
 
